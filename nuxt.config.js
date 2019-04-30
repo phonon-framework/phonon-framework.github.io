89bb8094d4c52913f19939cd135fb6e459d712c2
Qathom <tony9295@gmail.com>
2019-04-30 21:37:47 +0200

    Added Inter font

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,8 @@ module.exports = {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/github.min.css' }
+      { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/github.min.css' },
+      { rel: 'stylesheet', href: 'https://rsms.me/inter/inter.css' },
     ],
     script: [
       { src: '/js/phonon.js' },
