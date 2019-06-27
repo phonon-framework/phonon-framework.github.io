aa3263404877acdfaba2e3996d3d30da829a940c
Qathom <tony9295@gmail.com>
2019-06-28 01:27:36 +0200

    Added use cases

---
+++ use-cases/index.js
@@ -0,0 +1,23 @@
+export default [
+  /*
+   * ============================================
+   * Add your project to the use cases page!
+   * ============================================
+   *
+   * Rules:
+   * - The title must not exceed 60 characters
+   * - The description must not exceed 160 characters
+   * - Labels are optional
+   * - The homepage must be a valid URL
+   *
+   * Here is the "use case object", you can copy below example:
+    {
+      title: 'Phonon Framework',
+      description: 'Phonon is a front-end framework made with mobile and desktop in mind.',
+      labels: ['Framework', 'Web', 'Frontend'],
+      homepage: 'https://phonon-framework.github.io',
+    },
+  */
+
+  // Add your project below:
+];
