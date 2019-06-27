a138d54394df649d5603e3916e9bd55b14a8b5bf
Qathom <tony9295@gmail.com>
2019-06-28 01:28:05 +0200

    Created use case component

---
+++ components/UseCase.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div class="d-flex">
+      <div class="flex-grow-1">
+        <h2 v-text="data.title" />
+        <p
+          class="text-justify mr-2"
+          v-text="data.description"
+        />
+        <a
+          :href="data.homepage"
+          target="_blank"
+        >
+          {{ data.title }}
+          <fa icon="chevron-right" />
+        </a>
+      </div>
+      <img
+        v-if="data.imageSrc"
+        height="100"
+        :src="data.imageSrc"
+        :title="data.title"
+      >
+    </div>
+    <div class="mt-3">
+      <div
+        v-for="(label, index) in data.labels"
+        :key="index"
+        class="bg-light py-2 px-3 mr-3 mb-3 d-inline-block"
+      >
+        Hello
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    data: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
