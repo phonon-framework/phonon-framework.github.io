d7ad4d24869e12cd010de5a543b6666e0d63ca2b
Qathom <tony9295@gmail.com>
2019-07-17 21:02:25 +0200

    Added markdown

--- utils/markdown.js
+++ utils/markdown.js
@@ -44,6 +44,52 @@ import v200alpha1utilitiestextmd from '@/markdown/v2.0.0-alpha.1/utilities/text.
 import v200alpha1utilitiesunselectablemd from '@/markdown/v2.0.0-alpha.1/utilities/unselectable.md';
 import v200alpha1utilitiesverticalalignmd from '@/markdown/v2.0.0-alpha.1/utilities/vertical-align.md';
 import v200alpha1utilitiesvisibilitymd from '@/markdown/v2.0.0-alpha.1/utilities/visibility.md';
+import v200alpha2componentsaccordionmd from '@/markdown/v2.0.0-alpha.2/components/accordion.md';
+import v200alpha2componentsalertmd from '@/markdown/v2.0.0-alpha.2/components/alert.md';
+import v200alpha2componentsbuttongroupmd from '@/markdown/v2.0.0-alpha.2/components/button-group.md';
+import v200alpha2componentsbuttonmd from '@/markdown/v2.0.0-alpha.2/components/button.md';
+import v200alpha2componentscollapsemd from '@/markdown/v2.0.0-alpha.2/components/collapse.md';
+import v200alpha2componentscollectionmd from '@/markdown/v2.0.0-alpha.2/components/collection.md';
+import v200alpha2componentsfootermd from '@/markdown/v2.0.0-alpha.2/components/footer.md';
+import v200alpha2componentsformmd from '@/markdown/v2.0.0-alpha.2/components/form.md';
+import v200alpha2componentsgridmd from '@/markdown/v2.0.0-alpha.2/components/grid.md';
+import v200alpha2componentsindexmd from '@/markdown/v2.0.0-alpha.2/components/index.md';
+import v200alpha2componentslinemd from '@/markdown/v2.0.0-alpha.2/components/line.md';
+import v200alpha2componentsloadermd from '@/markdown/v2.0.0-alpha.2/components/loader.md';
+import v200alpha2componentsmodalmd from '@/markdown/v2.0.0-alpha.2/components/modal.md';
+import v200alpha2componentsnavbarmd from '@/markdown/v2.0.0-alpha.2/components/navbar.md';
+import v200alpha2componentsnotificationmd from '@/markdown/v2.0.0-alpha.2/components/notification.md';
+import v200alpha2componentsoffcanvasmd from '@/markdown/v2.0.0-alpha.2/components/off-canvas.md';
+import v200alpha2componentsprogressmd from '@/markdown/v2.0.0-alpha.2/components/progress.md';
+import v200alpha2componentsselectboxmd from '@/markdown/v2.0.0-alpha.2/components/selectbox.md';
+import v200alpha2componentstabmd from '@/markdown/v2.0.0-alpha.2/components/tab.md';
+import v200alpha2componentstablemd from '@/markdown/v2.0.0-alpha.2/components/table.md';
+import v200alpha2componentstilemd from '@/markdown/v2.0.0-alpha.2/components/tile.md';
+import v200alpha2gettingstartedcustombuildsmd from '@/markdown/v2.0.0-alpha.2/getting-started/custom-builds.md';
+import v200alpha2gettingstartedindexmd from '@/markdown/v2.0.0-alpha.2/getting-started/index.md';
+import v200alpha2gettingstartedmigrationmd from '@/markdown/v2.0.0-alpha.2/getting-started/migration.md';
+import v200alpha2gettingstartedquickstartmd from '@/markdown/v2.0.0-alpha.2/getting-started/quick-start.md';
+import v200alpha2gettingstartedthemingmd from '@/markdown/v2.0.0-alpha.2/getting-started/theming.md';
+import v200alpha2utilitiesbordersmd from '@/markdown/v2.0.0-alpha.2/utilities/borders.md';
+import v200alpha2utilitiesclearfixmd from '@/markdown/v2.0.0-alpha.2/utilities/clearfix.md';
+import v200alpha2utilitiescolorsmd from '@/markdown/v2.0.0-alpha.2/utilities/colors.md';
+import v200alpha2utilitiesdisplaymd from '@/markdown/v2.0.0-alpha.2/utilities/display.md';
+import v200alpha2utilitiesembedmd from '@/markdown/v2.0.0-alpha.2/utilities/embed.md';
+import v200alpha2utilitiesflexmd from '@/markdown/v2.0.0-alpha.2/utilities/flex.md';
+import v200alpha2utilitiesfloatmd from '@/markdown/v2.0.0-alpha.2/utilities/float.md';
+import v200alpha2utilitiesfontsmd from '@/markdown/v2.0.0-alpha.2/utilities/fonts.md';
+import v200alpha2utilitiesiconsmd from '@/markdown/v2.0.0-alpha.2/utilities/icons.md';
+import v200alpha2utilitiesindexmd from '@/markdown/v2.0.0-alpha.2/utilities/index.md';
+import v200alpha2utilitiesnetworkmd from '@/markdown/v2.0.0-alpha.2/utilities/network.md';
+import v200alpha2utilitiespositionmd from '@/markdown/v2.0.0-alpha.2/utilities/position.md';
+import v200alpha2utilitiesscreenreadersmd from '@/markdown/v2.0.0-alpha.2/utilities/screen-readers.md';
+import v200alpha2utilitiesshadowmd from '@/markdown/v2.0.0-alpha.2/utilities/shadow.md';
+import v200alpha2utilitiessizingmd from '@/markdown/v2.0.0-alpha.2/utilities/sizing.md';
+import v200alpha2utilitiesspacingmd from '@/markdown/v2.0.0-alpha.2/utilities/spacing.md';
+import v200alpha2utilitiestextmd from '@/markdown/v2.0.0-alpha.2/utilities/text.md';
+import v200alpha2utilitiesunselectablemd from '@/markdown/v2.0.0-alpha.2/utilities/unselectable.md';
+import v200alpha2utilitiesverticalalignmd from '@/markdown/v2.0.0-alpha.2/utilities/vertical-align.md';
+import v200alpha2utilitiesvisibilitymd from '@/markdown/v2.0.0-alpha.2/utilities/visibility.md';
 
 
 export default {
@@ -93,4 +139,50 @@ export default {
   'v2.0.0-alpha.1/utilities/unselectable': v200alpha1utilitiesunselectablemd,
   'v2.0.0-alpha.1/utilities/vertical-align': v200alpha1utilitiesverticalalignmd,
   'v2.0.0-alpha.1/utilities/visibility': v200alpha1utilitiesvisibilitymd,
+  'v2.0.0-alpha.2/components/accordion': v200alpha2componentsaccordionmd,
+  'v2.0.0-alpha.2/components/alert': v200alpha2componentsalertmd,
+  'v2.0.0-alpha.2/components/button-group': v200alpha2componentsbuttongroupmd,
+  'v2.0.0-alpha.2/components/button': v200alpha2componentsbuttonmd,
+  'v2.0.0-alpha.2/components/collapse': v200alpha2componentscollapsemd,
+  'v2.0.0-alpha.2/components/collection': v200alpha2componentscollectionmd,
+  'v2.0.0-alpha.2/components/footer': v200alpha2componentsfootermd,
+  'v2.0.0-alpha.2/components/form': v200alpha2componentsformmd,
+  'v2.0.0-alpha.2/components/grid': v200alpha2componentsgridmd,
+  'v2.0.0-alpha.2/components/index': v200alpha2componentsindexmd,
+  'v2.0.0-alpha.2/components/line': v200alpha2componentslinemd,
+  'v2.0.0-alpha.2/components/loader': v200alpha2componentsloadermd,
+  'v2.0.0-alpha.2/components/modal': v200alpha2componentsmodalmd,
+  'v2.0.0-alpha.2/components/navbar': v200alpha2componentsnavbarmd,
+  'v2.0.0-alpha.2/components/notification': v200alpha2componentsnotificationmd,
+  'v2.0.0-alpha.2/components/off-canvas': v200alpha2componentsoffcanvasmd,
+  'v2.0.0-alpha.2/components/progress': v200alpha2componentsprogressmd,
+  'v2.0.0-alpha.2/components/selectbox': v200alpha2componentsselectboxmd,
+  'v2.0.0-alpha.2/components/tab': v200alpha2componentstabmd,
+  'v2.0.0-alpha.2/components/table': v200alpha2componentstablemd,
+  'v2.0.0-alpha.2/components/tile': v200alpha2componentstilemd,
+  'v2.0.0-alpha.2/getting-started/custom-builds': v200alpha2gettingstartedcustombuildsmd,
+  'v2.0.0-alpha.2/getting-started/index': v200alpha2gettingstartedindexmd,
+  'v2.0.0-alpha.2/getting-started/migration': v200alpha2gettingstartedmigrationmd,
+  'v2.0.0-alpha.2/getting-started/quick-start': v200alpha2gettingstartedquickstartmd,
+  'v2.0.0-alpha.2/getting-started/theming': v200alpha2gettingstartedthemingmd,
+  'v2.0.0-alpha.2/utilities/borders': v200alpha2utilitiesbordersmd,
+  'v2.0.0-alpha.2/utilities/clearfix': v200alpha2utilitiesclearfixmd,
+  'v2.0.0-alpha.2/utilities/colors': v200alpha2utilitiescolorsmd,
+  'v2.0.0-alpha.2/utilities/display': v200alpha2utilitiesdisplaymd,
+  'v2.0.0-alpha.2/utilities/embed': v200alpha2utilitiesembedmd,
+  'v2.0.0-alpha.2/utilities/flex': v200alpha2utilitiesflexmd,
+  'v2.0.0-alpha.2/utilities/float': v200alpha2utilitiesfloatmd,
+  'v2.0.0-alpha.2/utilities/fonts': v200alpha2utilitiesfontsmd,
+  'v2.0.0-alpha.2/utilities/icons': v200alpha2utilitiesiconsmd,
+  'v2.0.0-alpha.2/utilities/index': v200alpha2utilitiesindexmd,
+  'v2.0.0-alpha.2/utilities/network': v200alpha2utilitiesnetworkmd,
+  'v2.0.0-alpha.2/utilities/position': v200alpha2utilitiespositionmd,
+  'v2.0.0-alpha.2/utilities/screen-readers': v200alpha2utilitiesscreenreadersmd,
+  'v2.0.0-alpha.2/utilities/shadow': v200alpha2utilitiesshadowmd,
+  'v2.0.0-alpha.2/utilities/sizing': v200alpha2utilitiessizingmd,
+  'v2.0.0-alpha.2/utilities/spacing': v200alpha2utilitiesspacingmd,
+  'v2.0.0-alpha.2/utilities/text': v200alpha2utilitiestextmd,
+  'v2.0.0-alpha.2/utilities/unselectable': v200alpha2utilitiesunselectablemd,
+  'v2.0.0-alpha.2/utilities/vertical-align': v200alpha2utilitiesverticalalignmd,
+  'v2.0.0-alpha.2/utilities/visibility': v200alpha2utilitiesvisibilitymd,
 };
